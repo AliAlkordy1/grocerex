24a2c644fd2e14de0555255aab1977fa579d9502
ali_alkordy <iraqigamer103@gmail.com>
2024-10-24 08:49:25 +0300

    1

--- src/components/dealOfTheMonth.jsx
+++ src/components/dealOfTheMonth.jsx
@@ -31,11 +31,11 @@ export default function DealOfTheMonth() {
             <div className="deal-of-the-month">
                 <img src="https://bestyet.com/wp-content/uploads/2021/05/21351_BY_1.5qt_Vanilla_Ice_Cream.png" alt="Ice Cream" />
                 <div className="deal-of-the-month-details">
-                    <h3>Deals of The Month</h3>
+                    <h3>Deal of The Month</h3>
                     <h2>Buy 1 Ice Cream Tub And Get 1 Free</h2>
                     <p>is simply dummy text of the printing and typesetting industry.<br /> Lorem Ipsum has been the industry`&apos`s standard.</p>
                     <div className="countdown">
-                        <p>Time Left:</p>
+                       
                         <p>Days: {timeLeft.days}</p>
                         <p>Hours: {timeLeft.hours}</p>
                         <p>Minutes: {timeLeft.minutes}</p>
